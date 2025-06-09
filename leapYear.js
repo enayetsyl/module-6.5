1f6ae808269ef6d1d8d2b52dce31d42d2cf6a060
enayetsyl <enayetflweb@gmail.com>
2025-06-09 10:07:29 +0600

    leap year done

---
+++ leapYear.js
@@ -0,0 +1,7 @@
+function isLeapYear(year){
+  return (year %4 === 0 && (year % 100 !== 0 || year % 400 === 0) )
+}
+
+console.log(isLeapYear(2024)); // true
+console.log(isLeapYear(1900)); // false
+console.log(isLeapYear(2000)); // true
